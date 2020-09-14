10934289ef8f0f367aa6a730df77a9e0fb3f3f88
Obese-Clown <43752986+Obese-Clown@users.noreply.github.com>
2020-09-14 07:34:52 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import com.github.obeseclown.tictactoe.examples.*;
+
+public class Main {
+
+  public static void main(String[] args)
+  {
+    AIGame game1 = new AIGame();
+    game1.start();
+    
+    //PlayerGame game2 = new PlayerGame();
+    //game2.start();
+  }
+}
